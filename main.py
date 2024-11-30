2f9a597f6933a371948cac08ca1aebaf02e16f6b
Augusto Hagemeier <augustolhagemeier@gmail.com>
2024-11-30 18:57:02 -0300

    i

--- main.py
+++ main.py
@@ -29,9 +29,7 @@ def Jogar():
             if letraDigitada in letrasTentadas: #Confere se a letra que ele escreveu não é uma repetida
                 print("Você já tentou essa letra. Tente outra!")
                 continue
-            
-            print(palavraAleatoria)
-            
+                        
             letrasTentadas.add(letraDigitada)
             if letraDigitada in palavraAleatoria: #Se a letra estiver na palavra ele adiciona a letra no indice i da palavra escondida trocando o valor "_" pela letra
                 print(f"A letra '{letraDigitada}' esta na palavra! ")
@@ -41,21 +39,39 @@ def Jogar():
                             
                 for letra in palavraEscondida: # printa a palavra escondida '_ _ _ _ _'       
                     print(letra, end=" ")  
-                print("")       
-                    
+                print("")              
             else:
+                print("..............")
                 print(f"A letra '{letraDigitada}' não está na palavra.")
-                print(f"Voce tem {erros} erro(s) ")
                 erros += 1
+                print(f"Você tem {erros} erro(s) ")
+                print("..............")
         else:
-            print("Voce perdeu!")
             print("")
-    print("Você ganhou! Parabéns")
-    print("-------------------------------")
+            print("Voce perdeu!")
+            print(f"A palavra era {palavraAleatoria}")
+            print("")
+            break
+    if "_" not in palavraEscondida:
+        print("-------------------------------")
+        print("Você ganhou! Parabéns")
+        print("-------------------------------")
+        
                     
-    
-    
-    
+def Ranking():
+    # Como fazer:
+    # Criar variaveis para contar a quantidade de chutes certos e errados dentro do metodo Jogar()
+    # A chave do dicionário vai ser o nome e o valor a razão de acertos ex: {"Augusto": 15}
+    # precisa fazer a conferencia para que se o nome já estver na lista só somar a quantidade de acertos, e de jogadas totais
+    # Provavelmente tem que fazer um outro dicionário que vai reeber a chave com o nome tambpem, mas com valor, jogadas totais algo assim:
+    #   ranking = {
+    #   "Alice": {"acertos": 25, "tentativas": 35},
+    #   "Bob": {"acertos": 18, "tentativas": 25}
+    #   } 
+    # para fazer a razão precisamos validar se o numero de tentativas vai ser diferente de 0
+    # A visualização do ranking vai ter q fazer o sort de acordo com a razão dos acertos e printar em desc
+    return
+
 
 i = ""
 while i != "4":
