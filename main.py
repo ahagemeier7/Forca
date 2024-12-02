4d85d5ed2da451f85b19871d034b1f66a1eaf5dd
Augusto Hagemeier <augustolhagemeier@gmail.com>
2024-12-02 20:12:01 -0300

    mudancas 2

--- main.py
+++ main.py
@@ -45,10 +45,10 @@ def Jogar():
                 continue
             tentativas += 1
                        
-            letrasTentadas.add(letraDigitada) #adiciona a letra na lista
+            letrasTentadas.append(letraDigitada) #adiciona a letra na lista
             
             if letraDigitada in palavraAleatoria: #Se a letra estiver na palavra ele adiciona a letra no indice i da palavra escondida trocando o valor "_" pela letra
-                print("\nA letra ", letraDigitada," esta na palavra! ")
+                print("\nA letra ", letraDigitada," está na palavra! ")
                 acertos += 1
 
                 for indice, char in enumerate(palavraAleatoria): # troca o _ pela letra digitada na posicao certa
@@ -67,7 +67,7 @@ def Jogar():
                 print(f"Você tem {erros} erro(s) ")
                 print("..............")
         else:
-            print("Voce perdeu!")
+            print("Você perdeu!")
             print("A palavra era ",palavraAleatoria)
             break
         
@@ -78,31 +78,55 @@ def Jogar():
         
     razaoDeacertos = round((acertos/tentativas)*100, 2)
         
-    if nomeDoJogador in ranking:
+    if nomeDoJogador in ranking: #atualiza os dados do jogador caso ele já tenha jogado antes ou cria um novo dicionario para ele
         ranking[nomeDoJogador]["acertos"] += acertos
         ranking[nomeDoJogador]["tentativas"] += tentativas
 
-        razaoDeacertos = round((ranking[nomeDoJogador]["acertos"]/ranking[nomeDoJogador]["tentativas"])*100, 2)
+        razaoDeacertos = round((ranking[nomeDoJogador]["acertos"]/ranking[nomeDoJogador]["tentativas"])*100, 2) 
         ranking[nomeDoJogador]["razao"] = razaoDeacertos
     else:
         ranking[nomeDoJogador] = {"acertos": acertos, "tentativas": tentativas, "razao": razaoDeacertos}
+
 
 #ranking = {
     # jogador1: {acerto: 2, tentativas: 5, razao: 20}
     # jogador2: {acerto: 5, tentativas: 9, razao: x}
 # }
       
+      
+ranking_completo = [] #Inicializa uma lista para colocar os itens
+def SalvaRanking():
+    
+    for nome, valores in ranking.items():  # Coloca todos os itens na lista
+        acertos = valores["acertos"]
+        tentativas = valores["tentativas"]
+        razao = valores["razao"]
+        ranking_completo.append((nome,acertos,tentativas, razao)) #salva os valores na lista para serem colocados no arquivo
+
+    with open("ranking.txt", "w") as arquivo:
+        for item in ranking_completo:
+            linha = ", ".join(map(str, item))  # Converte valor para uma string separada por virgulas, sendo os itens juntados pelo join()
+            arquivo.write(linha + "\n")  # Escreve cada valor como uma linha no arquivo
+
+
+def CarregaRanking():
+    ranking_completo.clear() 
+    with open("ranking.txt", "r") as arquivo:
+        for linha in arquivo:
+            if linha.strip():# valida se existe alguma linha no ranking.txt
+                elementos = linha.strip().split(", ") # Remove espaços e divide a linha 
+                nome = elementos[0]
+                acertos = int(elementos[1])
+                tentativas = int(elementos[2])
+                razao = float(elementos[3])
+                ranking_completo.append((nome,acertos,tentativas, razao)) # Adiciona o valor carregado para a lista
+
 def Ranking():
     print("\n=== Ranking ===")
-    ranking_completo = [] #Inicializa uma lista para colocar os itens
-    
-    for nome, valores in ranking.items(): #Coloca todos os itens na lista
-        razao = valores["razao"]
-        ranking_completo.append((nome, razao))
 
-    ranking_ordenado = sorted(ranking_completo, key=lambda x: x[1], reverse=True) #ordena a lista de forma decrescente, pelo item 3 da tupla
+    ranking_ordenado = sorted(ranking_completo, key=lambda x: x[3], reverse=True) #ordena a lista de forma decrescente, pelo item 3 da tupla
 
-    for i, (nome, razao) in enumerate(ranking_ordenado, start=1): #printa a lista em ordem, comecando pelo número 1
+    for i, (nome,acertos,tentativas, razao) in enumerate(ranking_ordenado, start=1): #printa a lista em ordem, comecando pelo número 1
         print(i,"º lugar: ", nome, " - ",  razao,"% de acerto")
 
 
@@ -118,12 +142,14 @@ def AdicionarPalavra():
             print("A palavra deve conter apenas letras!")
 
 
+CarregaRanking() # Garante que o ranking seja carregado corretamente ao reiniciar o programa
 i = ""
 while i != "4":
     print("\nDigite a opção desejada\n1 - Jogar\n2 - Ver Ranking\n3 - Adicionar palavra ao conjunto de palavras\n4 - sair")
     i = input()
     if i == "1":
         Jogar()
+        SalvaRanking() #salva  o ranking do jogador a cada jogada
     elif i == "2":
         Ranking()
     elif i == "3":
